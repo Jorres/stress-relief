55c69396436ffb533992873ddd52ebc55a7bc1d1
Jorres Tarasov <jorres.tarasov@gmail.com>
2020-12-09 14:44:44 +0300

    NEW: many new assets + registration form

--- src/wordCloud.js
+++ src/wordCloud.js
@@ -6,9 +6,8 @@ const updateWordCloud = (word) => {
     localStorage.setItem("words", words);
 };
 
-const getPicture = (pictureWrapper, pictureElem) => {
-    console.log(localStorage.getItem("words"));
-    fetch("https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud", {
+const getPicture = (pictureWrapper, text) => {
+    return fetch("https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud", {
         method: "POST",
         headers: {
             "x-rapidapi-host": "textvis-word-cloud-v1.p.rapidapi.com",
@@ -17,24 +16,19 @@ const getPicture = (pictureWrapper, pictureElem) => {
             accept: "application/json"
         },
         body: JSON.stringify({
-            text:
-            localStorage.getItem("words"),
+            text: text,
             scale: 1,
             width: pictureWrapper.offsetWidth,
             height: pictureWrapper.offseHeight,
             colors: ["#375E97", "#FB6542", "#FFBB00", "#3F681C"],
             font: "Tahoma",
-            use_stopwords: true,
+            use_stopwords: false,
             language: "en",
             uppercase: false
         })
     })
         .then(response => {
             return response.text();
-        })
-        .then(wordCloud => {
-            console.log(wordCloud);
-            pictureElem.src = wordCloud;
         })
         .catch(err => {
             console.log(err);
